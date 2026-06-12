865fa1839a62883ece7374fe47795a9b71c8f7cd
Andrey <andrii@a2g.name>
2026-06-13 00:45:35 +0300

    added web projects for AOT and JIT

--- src/Chat.Jit.Web/Program.cs
+++ src/Chat.Jit.Web/Program.cs
@@ -1,6 +1,18 @@
+using Chat.Shared;
+using Microsoft.AspNetCore.SignalR;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+builder.Services.Configure<JsonHubProtocolOptions>(options =>
+{
+    options.PayloadSerializerOptions.TypeInfoResolverChain.Insert(0, ChatJsonContext.Default);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => Results.Content(IndexHtml.Content, "text/html"));
+app.MapGet("/ready", () => Results.Text("ready"));
+app.MapHub<ChatHub>("/chat");
 
 app.Run();
